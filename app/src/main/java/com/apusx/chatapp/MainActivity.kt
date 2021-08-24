79c0693826618765a56498c408829ce3f763d70e
Andrey Sergeev <wantedmins@gmail.com>
2021-08-24 14:46:47 +0300

    Add Cht

--- app/src/main/java/com/apusx/chatapp/MainActivity.kt
+++ app/src/main/java/com/apusx/chatapp/MainActivity.kt
@@ -1,11 +1,128 @@
 package com.apusx.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import com.apusx.chatapp.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    fun String.isValidEmail(): Boolean
+            = this.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    private lateinit var auth: FirebaseAuth
+
+    val TAG = "TEST"
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 1021
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = Firebase.auth
+        binding.signinbtn.setOnClickListener {
+            if(binding.email.text.isNotEmpty()){
+                if(!binding.email.text.toString().isValidEmail()){
+                    Toast.makeText(this,"Email Invalid",Toast.LENGTH_LONG).show()
+                }else{
+                    if(binding.password.text.isEmpty())
+                        Toast.makeText(this,"Password Invalid",Toast.LENGTH_LONG).show()
+                    else{
+                        Toast.makeText(this,"OK",Toast.LENGTH_LONG).show()
+
+                        auth.signInWithEmailAndPassword(binding.email.text.toString(), binding.password.text.toString())
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success")
+                                    val user = auth.currentUser
+                                    Toast.makeText(this,user?.uid.toString(),Toast.LENGTH_LONG).show()
+                                    startActivity(Intent(this,ChatActivity::class.java))
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                                    Toast.makeText(baseContext, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show()
+
+                                }
+                            }
+                    }
+                }
+            }else
+                Toast.makeText(this,"Email Invalid",Toast.LENGTH_LONG).show()
+
+        }
+
+        binding.googlebtn.setOnClickListener() {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+
+        binding.signupbtn.setOnClickListener() {
+            startActivity(Intent(this,SignUP::class.java))
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                }
+            }
+    }
+
 }
---
+++ app/src/main/java/com/apusx/chatapp/SignUP.kt
@@ -0,0 +1,86 @@
+package com.apusx.chatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.apusx.chatapp.databinding.ActivityMainBinding
+import com.apusx.chatapp.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignUP : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    private lateinit var auth: FirebaseAuth
+
+    val TAG = "TEST"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        auth = Firebase.auth
+
+
+        binding.signupsig.setOnClickListener() {
+
+            if (binding.name.text.toString().isEmpty())
+                Toast.makeText(this, "Name Invalid", Toast.LENGTH_LONG).show()
+            else
+                if (binding.emailsig.text.toString().isEmpty())
+                    Toast.makeText(this, "Email Invalid", Toast.LENGTH_LONG).show()
+                else
+                    if (binding.passwordsig.text.toString().isEmpty())
+                        Toast.makeText(this, "Pass Invalid", Toast.LENGTH_LONG).show()
+                    else
+                        if (binding.passwordconfsig.text.toString().isEmpty())
+                            Toast.makeText(this, "ConfPass Invalid", Toast.LENGTH_LONG).show()
+                        else
+                            if (!binding.passwordsig.text.toString()
+                                    .equals(binding.passwordconfsig.text.toString())
+                            )
+                                Toast.makeText(this, "ConfPass Wrong", Toast.LENGTH_LONG).show()
+                            else {
+                                auth.createUserWithEmailAndPassword(
+                                    binding.emailsig.text.toString(),
+                                    binding.passwordsig.text.toString()
+                                )
+                                    .addOnCompleteListener(this) { task ->
+                                        if (task.isSuccessful) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Log.d(TAG, "createUserWithEmail:success")
+                                            val user = auth.currentUser
+                                            Toast.makeText(this,user?.uid.toString(),Toast.LENGTH_LONG).show()
+                                            startActivity(Intent(this,ChatActivity::class.java))
+
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Log.w(
+                                                TAG,
+                                                "createUserWithEmail:failure",
+                                                task.exception
+                                            )
+                                            Toast.makeText(
+                                                baseContext, "Authentication failed.",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+
+                                        }
+                                    }
+                            }
+        }
+
+
+        binding.cancel.setOnClickListener() {
+            finish()
+        }
+
+
+    }
+}
